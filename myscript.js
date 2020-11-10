34f2e8e948192883884ebc47836bd4aef78ba853
StefanoSerafini <stefanoserafini7@gmail.com>
2020-11-10 16:06:50 +0100

    impostazione js

---
+++ myscript.js
@@ -0,0 +1,30 @@
+const todolist = new Vue ({
+    el: '#tot',
+
+    data: {
+        lista: [
+            {
+
+            },
+            {
+
+            },
+            {
+
+            },
+            {
+
+            },
+        ],
+
+        nuovaAttivita: ''
+    },
+
+    methods: {
+        aggAttivita() {
+        },
+        cancAttivita() {
+        }
+    }
+
+});
